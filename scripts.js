6d73da689028285cd4d77d3dcb03a7a7200be346
trentonasmith <112363306+trentonasmith@users.noreply.github.com>
2022-10-16 18:29:41 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// JavaScript for TP7
+
+
+window.onload = funtion() {
+   
+document.querySelector("#titleBanner h1").classList.add("tp")
+
+document.querySelector("#titleBanner h1").onclick = function() {
+    
+}
+
+} // end window.onload 
